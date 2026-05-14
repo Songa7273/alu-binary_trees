e81faa8dced99e394332825fd8a9d14836c58cf8
Songa7273 <songa7273@example.com>
2026-05-14 03:25:20 -0700

    Task 0: create binary tree node

---
+++ binary_trees.h
@@ -0,0 +1,26 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ * @n: Integer stored in the node
+ * @parent: Pointer to parent node
+ * @left: Pointer to left child
+ * @right: Pointer to right child
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
